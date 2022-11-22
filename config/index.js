796276897045e3da0c891013af73ae63725ebf2f
Cindy <cindychynoweth@hotmail.com>
2022-11-21 19:44:02 -0700

    File Updates

---
+++ config/index.js
@@ -0,0 +1,4 @@
+const login = require("../../../login");
+// const Thought = require("./Thought");
+
+module.exports = { login};
---
+++ routes/api/destinations.js
@@ -0,0 +1,13 @@
+// const router = require("express").Router();
+// const tripsController = require("../../controllers/destinations")
+
+
+// router
+//     .route("/")
+//     .get(destinationsController.getAllTrips)
+//     .post(destinationsController.createTrip);
+
+// router
+//     .route("/:id")
+//     .get(destinationsController.getOneDestinations);
+  
