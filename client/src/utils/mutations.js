ff0dc8924aef87984f657697e8f8310bf65a2c62
Morgan Woods <morgan4242564@gmail.com>
2022-11-24 20:56:01 -0700

    added search bar, about us, and linked APIs

--- client/src/utils/mutations.js
+++
@@ -1,9 +0,0 @@
-import { gql } from '@apollo/client';
-export const LOCATION_SEARCH = gql`
-  mutation loactionSearch($lat: String, $lon: String) {
-    searchLocation(lat: $lat, lon: $lon) {
-      lat
-      lon
-    }
-  }
-`;
--- client/src/utils/queries.js
+++
@@ -1,9 +0,0 @@
-import { gql } from "@apollo/client";
-export const GET_LOCATION = gql`
-{
-    me{
-            lat
-            lon
-    }
-}
-`;
